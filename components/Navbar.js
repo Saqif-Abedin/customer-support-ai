aa1ad7890b0d6304f92f75c7ffb7d8c1ab7504a1
omar-t-ahmed <omartahmed@gmail.com>
2024-08-06 22:34:41 -0400

    update styling bg

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ import MenuIcon from "@mui/icons-material/Menu";
 
 const Navbar = () => {
   return (
-    <AppBar position="static">
+    <AppBar position="static" color="primary">
       <Toolbar>
         <IconButton edge="start" color="inherit" aria-label="menu">
           <MenuIcon />
