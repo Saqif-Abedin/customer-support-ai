bb1ccdb939d815b4aa31d5483c426d03db6a5ef0
hasan-huda <hasanhuda037@gmail.com>
2024-08-06 21:07:32 -0400

    Navbar and styling

---
+++ components/Navbar.js
@@ -0,0 +1,21 @@
+"use client";
+
+import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
+const Navbar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" style={{ flexGrow: 1, textAlign: 'center' }}>
+          Tablighi Titans Support Page
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
